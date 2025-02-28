4c1661e664780ccef2ae23a5eea5a1859c435efd
esmond <112294683+esmondherry@users.noreply.github.com>
2025-02-28 14:37:49 -0500

    spilt up main into functions

--- main.py
+++ main.py
@@ -1,37 +1,36 @@
 import json
 from dotenv import load_dotenv
 
-from haystack import Pipeline,Document
+from haystack import Pipeline, Document
 from haystack.document_stores.in_memory import InMemoryDocumentStore
 from haystack.components.retrievers.in_memory import InMemoryBM25Retriever
 from haystack.components.generators import OpenAIGenerator
 from haystack.components.builders.prompt_builder import PromptBuilder
 
+load_dotenv()
+
+
 def create_documents(articleJSON):
-    with open(articleJSON, 'r') as f:
+    with open(articleJSON, "r") as f:
         articles = json.load(f)
 
     documents = []
     for article in articles:
         doc = Document(
-            content=article['abstract'],
+            content=article["abstract"],
             meta={
-                'pmid': article['pmid'],
-                'authors': article['authors'],
-                'title': article['title'],
-                'publication_year': article['publication_year']
-            }
+                "pmid": article["pmid"],
+                "authors": article["authors"],
+                "title": article["title"],
+                "publication_year": article["publication_year"],
+            },
         )
         documents.append(doc)
-    return documents    
+    return documents
 
 
-def main():
-    articleJSON = 'data.json'
-    
-    load_dotenv()
-    documents = create_documents(articleJSON)
-
+def setup_AI(articles_path):
+    documents = create_documents(articles_path)
     document_store = InMemoryDocumentStore()
     document_store.write_documents(documents)
 
@@ -47,11 +46,11 @@ def main():
     Answer:
     """
 
-
     retriever = InMemoryBM25Retriever(document_store=document_store)
     prompt_builder = PromptBuilder(template=prompt_template)
     llm = OpenAIGenerator()
 
+    global rag_pipeline
     rag_pipeline = Pipeline()
     rag_pipeline.add_component("retriever", retriever)
     rag_pipeline.add_component("prompt_builder", prompt_builder)
@@ -59,14 +58,26 @@ def main():
     rag_pipeline.connect("retriever", "prompt_builder.documents")
     rag_pipeline.connect("prompt_builder", "llm")
 
-    question = input("question: ")
+
+def ask_AI(question):
     results = rag_pipeline.run(
         {
             "retriever": {"query": question},
             "prompt_builder": {"question": question},
         }
     )
-    print(results["llm"]["replies"][0],"\n")
+    response = results["llm"]["replies"][0]
+    return response
+
+
+def main():
+    articleJSON = "data.json"
+    setup_AI(articleJSON)
+
+    question = input("question: ")
+    answer = ask_AI( question)
+
+    print(answer)
 
 
 if __name__ == "__main__":
